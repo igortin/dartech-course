f695b73c91be4b7a1da6cd91e1f547d80696d821
itin <tin.igrik@gmail.com>
2019-07-16 15:02:52 +0600

    delete empty lines

--- 01_interface.go
+++ 01_interface.go
@@ -8,24 +8,17 @@ import (
 func main(){
 	b :=Box{}
 	b.Put(1,2,3,5,"Alex",9,999)
-	fmt.Printf("slice : %v\n", b.interfaces)
 	b.Drop("Alex")
-	fmt.Printf("slice : %v\n", b.interfaces)
-
-
 }
-
-
 
 type iBox interface{
 	Put(...interface{}) // ... variate argument BOx.Put(1,2,5,7,"Peter")
 	Drop(interface{})
 }
-// generic array save int string and we need create type with realize interface Box
+
 type Box struct {
 	interfaces []interface{}
 }
-
 
 func (b *Box) Put(a ...interface{}) {
 	for _,val := range a {
