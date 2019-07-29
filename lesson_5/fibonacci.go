4349685d83d0c8df379458ae26afa8aacc17e959
Igor Tin <itin@dar.kz>
2019-07-29 22:29:33 +0600

    hw 5 task

--- lesson_5/fibonacci.go
+++ lesson_5/fibonacci.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"sort"
 	"strconv"
 )
 
@@ -16,10 +17,38 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic("request has no parameter id")
 	}
+
 	if num > 0 {
-		a, b, c := getSlice(num + 1)
-		fb := &FibNumber{a, b, c}
+
+		slice := []int{}
+
+		ch := make(chan int)
+		defer close(ch)
+
+		go func(ch chan<- int) {
+			ch <- fib(num - 1)
+		}(ch)
+
+		go func(ch chan<- int) {
+			ch <- fib(num)
+		}(ch)
+
+		go func(ch chan<- int) {
+			ch <- fib(num + 1)
+		}(ch)
+
+		for i := 0; i < 3; i++ {
+			slice = append(slice, <-ch)
+		}
+
+		sort.Slice(slice, func(i, j int) bool {
+			return slice[i] < slice[j]
+		})
+
+		fb := &FibNumber{slice[0], slice[1], slice[2]}
+
 		fbj, _ := json.Marshal(fb)
+
 		_, _ = w.Write(fbj)
 	} else {
 		_, _ = w.Write([]byte("Wrong number! please try again!"))
@@ -32,31 +61,21 @@ type FibNumber struct {
 	Next    int
 }
 
+
+
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/fib/{id}", handler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func fib(p int, m map[int]int) int {
+func fib(p int) int {
 	if p == 1 || p == 2 {
-		m[p] = 1
 		return 1
 	}
 	if p == 0 {
-		m[p] = 0
 		return 0
 	}
-	if w, t := m[p]; t {
-		return w
-	} else {
-		m[p] = fib(p-1, m) + fib(p-2, m)
-	}
-	return m[p]
-}
-
-func getSlice(p int) (int, int, int) {
-	m := make(map[int]int)
-	fib(p+1, m)
-	return m[p-2], m[p-1], m[p]
+	return fib(p-1) + fib(p-2)
 }
